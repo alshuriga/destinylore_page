0e8e84095b365b8f5dfd70057571b823c396621e
alshuriga <alshuriga@gmail.com>
2020-12-01 16:10:28 +0100

    added loading img

--- static/js/main.js
+++ static/js/main.js
@@ -1,104 +1,103 @@
 var locale = 'en';
 
 $(document).ready(function() {
-
-	$(function() {
+    $(function() {
         $( window ).resize(function() {
-         centerApp();
-     });
+           centerApp();
+       });
 
         locale = $(location).attr('href').split('/').pop();
-        
-       //locale = 'ru';
 
-       $.ajax({
-        url: 'https://www.destinylib.xyz/req?id=' + locale,
-        type: 'GET',
-        dataType: 'json',
-        success: function(data, status, xhr)
-        {
+  locale = 'ru';
 
-            var root = 'https://www.destinylib.xyz/static/books/';
-            var pointer = 0;
+  $.ajax({
+    url: 'https://www.destinylib.xyz/req?id=' + locale,
+    type: 'GET',
+    dataType: 'json',
+    success: function(data, status, xhr)
+    {
 
-            for (i = 0; i < data.length; i++) {
-                $(".book-list-section").append('<button id="' + i + '"class="bookname button w-100">'+ data[i].name + '</button>');
-                $(data[i].name).preload();
+        var root = 'https://www.destinylib.xyz/static/books/';
+        var pointer = 0;
+
+        for (i = 0; i < data.length; i++) {
+            $(".book-list-section").append('<button id="' + i + '"class="bookname button w-100">'+ data[i].name + '</button>');
+            $(data[i].name).preload();
+        }
+        filter(data, pointer);
+        navCheck(pointer, data);
+        setTitles(data);
+        centerApp();
+        $('.bookname').first().addClass("active");
+        set_image(data[$('.active').first().attr('id')].cover);
+        $('#bookname').text(data[$('.active').first().attr('id')].name);
+        $('.book-controls a:first').attr('href', root + locale + '/'+ data[$('.active').first().attr('id')].group + '/' + data[$('.active').first().attr('id')].filename);
+        $(".ziplink").attr('href', root + 'destiny_lore_ebooks_' + locale + '.zip');
+        $('.bookname').mouseenter ( function() {
+
+            updateInfo(data,$(this).attr('id'), root);
+        });
+
+        $('.bookname').mouseleave (function () {
+            var id = $('.active').first().attr('id');
+            updateInfo(data, id, root);
+        });
+
+        $('.bookname').click( function() {
+            $('.bookname').removeClass("active");
+            $(this).addClass("active");
+            if($(window).width() < 990) {
+                $('html, body').animate({
+                    scrollTop: $(".book-info").first().offset().top
+                }, 500);
             }
+        })
+        ;
+
+        $('.list-section-item').click(function() {
+            $('.list-section-item').removeClass('active-group');
+            $(this).addClass('active-group');
+            pointer = 0;
             filter(data, pointer);
             navCheck(pointer, data);
-            setTitles(data);
-            centerApp();
-            $('.bookname').first().addClass("active");
-            set_image(data[$('.active').first().attr('id')].cover);
-            $('#bookname').text(data[$('.active').first().attr('id')].name);
-            $('.book-controls a:first').attr('href', root + locale + '/'+ data[$('.active').first().attr('id')].group + '/' + data[$('.active').first().attr('id')].filename);
-            $(".ziplink").attr('href', root + 'destiny_lore_ebooks_' + locale + '.zip');
-            $('.bookname').mouseenter ( function() {
-
-                updateInfo(data,$(this).attr('id'), root);
-            });
-
-            $('.bookname').mouseleave (function () {
-                var id = $('.active').first().attr('id');
-                updateInfo(data, id, root);
-            });
-
-            $('.bookname').click( function() {
-                $('.bookname').removeClass("active");
-                $(this).addClass("active");
-                 if($(window).width() < 990) {
-                    $('html, body').animate({
-                        scrollTop: $(".book-info").first().offset().top
-                    }, 500);
-                }
-            })
-            ;
-
-            $('.list-section-item').click(function() {
-                $('.list-section-item').removeClass('active-group');
-                $(this).addClass('active-group');
-                pointer = 0;
-                filter(data, pointer);
-                navCheck(pointer, data);
-                $('.bookname').removeClass("active");''
-                $('.bookname:visible').first().addClass("active");
-                var id = $('.bookname:visible').first().attr('id');
-                updateInfo(data, id, root);
-               
-
-            });
-
-            $('.down').click(function() {
-
-                pointer+=7;
-                filter(data, pointer);
-                navCheck(pointer, data);
-
-            });
+            $('.bookname').removeClass("active");''
+            $('.bookname:visible').first().addClass("active");
+            var id = $('.bookname:visible').first().attr('id');
+            updateInfo(data, id, root);
 
 
-            $('.up').click(function() {
+        });
 
-                pointer-=7;
-                filter(data, pointer);
-                navCheck(pointer, data);
-            });
+        $('.down').click(function() {
+
+            pointer+=7;
+            filter(data, pointer);
+            navCheck(pointer, data);
+
+        });
+
+
+        $('.up').click(function() {
+
+            pointer-=7;
+            filter(data, pointer);
+            navCheck(pointer, data);
+        });
 
 
 
 
-        },
+    },
 
-        error: function(xhr, status, error)
-        {
+    error: function(xhr, status, error)
+    {
 
-        }
-    });
+    }
+});
 
 
 
-   });
+});
 
 
 });
@@ -122,20 +121,21 @@ function filter(data, a) {
 
 function numberOfBooks(data) {
 
-  var currentGroup = $(".active-group").children().first().attr("id");
-  var number = 0;
-  $(".bookname").each(function(){
-    if (data[$(this).attr("id")].group == currentGroup) {
-      number++;
-  }
-});
-  return number;
+    var currentGroup = $(".active-group").children().first().attr("id");
+    var number = 0;
+    $(".bookname").each(function(){
+        if (data[$(this).attr("id")].group == currentGroup) {
+          number++;
+      }
+  });
+    return number;
 }
 
 
 function set_image(image) {
-	//$(".parent").append('<img id = "img" src = "' + image + '" >');
-	$(".book-logo").prepend('<img id = "cover-img" src="' + image + '" alt="" width="298px">');
+//$(".parent").append('<img id = "img" src = "' + image + '" >');
+$(".book-logo").prepend('<img id = "cover-img" src="' + image + '" alt="" width="298px">');
+
 }
 
 
@@ -144,9 +144,9 @@ function set_image(image) {
 function preload(arrayOfImages) {
     $(arrayOfImages).each(function(){
         $('<img/>')[0].src = this;
-        // Alternatively you could use:
-        // (new Image()).src = this;
-    });
+    // Alternatively you could use:
+    // (new Image()).src = this;
+});
 }
 
 function navCheck(pointer, data) {
@@ -156,12 +156,13 @@ function navCheck(pointer, data) {
         $('.down').addClass("inactive-scroll");
     }
     else if (pointer <= 0){
-     $('.up').addClass("inactive-scroll");
- }
+       $('.up').addClass("inactive-scroll");
+   }
 }
 
 function updateInfo(data, i, dir, root) {
     $('#cover-img').replaceWith('<img id = "cover-img" src="' + data[i].cover + '" alt="" width="298px">');
+   
     $('#bookname').text(data[i].name);
     $('.book-controls a:first').attr('href',dir  + locale + '/' + data[i].group + '/' + data[i].filename);
 }
@@ -191,12 +192,12 @@ function setTitles(data) {
 }
 
 function centerApp() {
-   if($(window).width() > 990 && $(".container").first().height()<$(window).height()) {
-    $(".container").addClass("center");
-}
-else {
- $(".container").removeClass("center");
-}
+    if($(window).width() > 990 && $(".container").first().height()<$(window).height()) {
+        $(".container").addClass("center");
+    }
+    else {
+        $(".container").removeClass("center");
+    }
 }
 // Usage:
 
