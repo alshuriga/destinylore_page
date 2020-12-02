de29fee810fd7e0436251747e1d104f5146f73d5
alshuriga <alshuriga@gmail.com>
2020-12-02 01:46:35 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+
+import flask
+
+from flask import request, jsonify, render_template, redirect
+
+
+import json
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/ru')
+@app.route('/en')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/req', methods=['GET'])
+def req():
+
+    if 'id' in request.args:
+        id = request.args['id']
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    with open('/home/alshuriga/mysite/_json/'+str(id)+'.json', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        response = jsonify(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # your processing here
+    return redirect('/en')
+
+
