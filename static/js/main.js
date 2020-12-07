292fa1a744e45426a46e1aebfa6df4ef0eefce46
alshuriga <alshuriga@gmail.com>
2020-12-07 20:55:26 +0100

    change lang without refresh

--- static/js/main.js
+++ static/js/main.js
@@ -11,112 +11,16 @@ $(document).ready(function() {
             locale = 'de';
         }
 
+           $('.dropdown-content').children().click( function() {
 
-  $.ajax({
-    url: 'https://www.destinylib.xyz/req?id=' + locale,
-    type: 'GET',
-    dataType: 'json',
-    success: function(data, status, xhr)
-    {
+                locale = $(this).attr('locale');
+                main(locale);
 
-        var root = 'https://www.destinylib.xyz//static/books/';
-        var pointer = 0;
+            });
 
-        for (i = 0; i < data.length; i++) {
-            var str = data[i].name.replace('[ap]',"'").replace('[dot]',".").replace("[dd]",":");
-            if (str.length > 30) {
-                 str = str.slice(0, 27) + '...';           
-            }
-            $(".book-list-section").append('<button id="' + i + '"class="bookname button w-100">'+ str + '</button>');
-            $(data[i].name).preload();
-        }
-        filter(data, pointer);
-        navCheck(pointer, data);
-        setTitles(data);
-        centerApp();
-        translate(locale);
-        $('#ddlang').text(locale);
-        $('.bookname').first().addClass("active");
-        set_image(data[$('.active').first().attr('id')].cover);
-        $('#bookname').text(data[$('.active').first().attr('id')].name);
-        $('.book-controls a:first').attr('href', root + locale + '/'+ data[$('.active').first().attr('id')].group + '/' + data[$('.active').first().attr('id')].filename);
-        $(".ziplink").attr('href', root + 'destiny_lore_ebooks_' + locale + '.zip');
-        $('.bookname').mouseenter ( function() {
+        main(locale);
 
-            updateInfo(data,$(this).attr('id'), root);
-        });
-
-        $('.bookname').mouseleave (function () {
-            var id = $('.active').first().attr('id');
-            updateInfo(data, id, root);
-        });
-
-        $('.bookname').click( function() {
-            $('.bookname').removeClass("active");
-            $(this).addClass("active");
-            if($(window).width() < 990) {
-                $('html, body').animate({
-                    scrollTop: $(".book-info").first().offset().top
-                }, 500);
-            }
-        })
-        ;
-
-        $('.blink').each(function() {
-        var elem = $(this);
-        setInterval(function() {
-        if (elem.css('opacity') == '0') {
-            elem.css('opacity', '1');
-        } else {
-            elem.css('opacity', '0');
-        }    
-         }, 500);
-        });
-
-        $('.list-section-item').click(function() {
-            $('.list-section-item').removeClass('active-group');
-            $(this).addClass('active-group');
-            pointer = 0;
-            filter(data, pointer);
-            navCheck(pointer, data);
-            $('.bookname').removeClass("active");''
-            $('.bookname:visible').first().addClass("active");
-            var id = $('.bookname:visible').first().attr('id');
-            updateInfo(data, id, root);
-
-
-        });
-
-        $('.down').click(function() {
-
-            pointer+=7;
-            filter(data, pointer);
-            navCheck(pointer, data);
-
-        });
-
-
-        $('.up').click(function() {
-
-            pointer-=7;
-            filter(data, pointer);
-            navCheck(pointer, data);
-        });
-
-
-
-
-    },
-
-    error: function(xhr, status, error)
-    {
-
-    }
-});
-
-
-
-});
+    });
 
 
 });
@@ -152,8 +56,13 @@ function numberOfBooks(data) {
 
 
 function set_image(image) {
-//$(".parent").append('<img id = "img" src = "' + image + '" >');
-$(".book-logo").prepend('<img id = "cover-img" src="' + image + '" alt="" width="298px">');
+    if ($(".book-logo").children().length){
+        $(".book-logo").children().first().attr('src', image);
+    }
+    else {
+        $(".book-logo").append('<img id = "cover-img" src="' + image + '" alt="" width="298px">');
+
+    }
 
 }
 
@@ -189,8 +98,8 @@ function updateInfo(data, i, dir, root) {
         $(".book-controls").css("margin-top",'70px');
     }
     else {
-         $(".book-controls").css("margin-top",'90px');
-    }
+     $(".book-controls").css("margin-top",'90px');
+ }
 }
 
 $.fn.preload = function() {
@@ -229,13 +138,124 @@ function centerApp() {
 
 function translate(locale) { 
 
-if (locale == 'ru') {
-    $('#desc span:first').text("лор destiny в e-book формате");
-    $('#language').text("../язык: ");
-    $('.ziplink').first().text("СКАЧАТЬ ВСЕ КНИГИ (ZIP-АРХИВ)");
-    $('#dontext').text("Хочешь поддержать destinylib? ");
-    $('#donate').text("ДОНАТ <3");
-}
-else return false;
+    if (locale != 'ru') {
+        $('#desc span:first').text("DESTINY LORE IN E-BOOK FORMAT");
+        $('#language').text("../LANGUAGE: ");
+        $('.ziplink').first().text("DOWNLOAD ALL (*.ZIP)");
+        $('#dontext').text("WANNA SUPPORT DESTINYLIB?");
+        $('#donate').text("DONATE <3");
+    }
+    else {
+        $('#desc span:first').text("лор destiny в e-book формате");
+        $('#language').text("../язык: ");
+        $('.ziplink').first().text("СКАЧАТЬ ВСЕ КНИГИ (ZIP-АРХИВ)");
+        $('#dontext').text("Хочешь поддержать destinylib? ");
+        $('#donate').text("ДОНАТ <3");
+    }
+    
 }
 
+function loadlist(data) {
+
+    for (i = 0; i < data.length; i++) {
+        var str = data[i].name.replace('[ap]',"'").replace('[dot]',".").replace("[dd]",":");
+        if (str.length > 30) {
+         str = str.slice(0, 27) + '...';           
+     }
+     $(".book-list-section").append('<button id="' + i + '"class="bookname button w-100">'+ str + '</button>');
+     $(data[i].name).preload();
+ }
+}
+
+
+function main(locale) {
+    $.ajax({
+        url: 'https://www.destinylib.xyz/req?id=' + locale,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data, status, xhr)
+        {
+            $(".bookname").remove();
+            var root = 'https://www.destinylib.xyz/static/books/';
+            var pointer = 0;
+            loadlist(data);
+            filter(data, pointer);
+            navCheck(pointer, data);
+            setTitles(data);
+            centerApp();
+            translate(locale);
+            $('#ddlang').text(locale);
+            $('.list-section-item').removeClass('active-group');
+            $('.list-section-item').first().addClass("active-group");
+            $('.bookname').first().addClass("active");
+
+            set_image(data[$('.active').first().attr('id')].cover);
+            $('#bookname').text(data[$('.active').first().attr('id')].name);
+            $('.book-controls a:first').attr('href', root + locale + '/'+ data[$('.active').first().attr('id')].group + '/' + data[$('.active').first().attr('id')].filename);
+            $(".ziplink").attr('href', root + 'destiny_lore_ebooks_' + locale + '.zip');
+            $('.bookname').mouseenter ( function() {
+                updateInfo(data,$(this).attr('id'), root);
+            });
+
+            $('.bookname').mouseleave (function () {
+                var id = $('.active').first().attr('id');
+                updateInfo(data, id, root);
+            });
+
+            $('.bookname').click( function() {
+                $('.bookname').removeClass("active");
+
+                $(this).addClass("active");
+                if($(window).width() < 990) {
+                    $('html, body').animate({
+                        scrollTop: $(".book-info").first().offset().top
+                    }, 500);
+                }
+            })
+
+           
+
+            $('.list-section-item').click(function() {
+                $('.list-section-item').removeClass('active-group');
+                $(this).addClass('active-group');
+                pointer = 0;
+                filter(data, pointer);
+                navCheck(pointer, data);
+                $('.bookname').removeClass("active");''
+                $('.bookname:visible').first().addClass("active");
+                var id = $('.bookname:visible').first().attr('id');
+                updateInfo(data, id, root);
+
+
+            });
+
+            $('.down').click(function() {
+
+                pointer+=7;
+                filter(data, pointer);
+                navCheck(pointer, data);
+
+            });
+
+
+            $('.up').click(function() {
+
+                pointer-=7;
+                filter(data, pointer);
+                navCheck(pointer, data);
+            });
+
+            console.log('done');
+
+
+        },
+
+        error: function(xhr, status, error)
+        {
+
+        }
+
+
+
+    });
+}
